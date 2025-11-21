d801fce54a02da0816b08173cd4f9067d4f3f009
vikasjaganath <vikashj05122006@gmail.com>
2025-11-21 14:32:00 +0530

    Update prog2.py

--- prog2.py
+++ prog2.py
@@ -1,7 +1,7 @@
-# per hour cost 0.51$ and we want find per day, per week,per month and 918$  
-# 918/0.51 = x hours
+
+
 perHoursCost = 0.51
-per_daycost = 0.51 * 24
+per_daycost = perHoursCost * 24
 print("per day cost",per_daycost)
 per_month = perHoursCost * per_daycost
 per_week = per_daycost * 7
@@ -10,4 +10,5 @@ hours_for_given_amount = give_amount/perHoursCost
 day_for_give_amount = hours_for_given_amount/24
 print("Per week cost:", per_week)
 print("per month cost:",per_month)
+
 print("for the given amount the server will run for:", day_for_give_amount)
